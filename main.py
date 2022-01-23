426537a4a09143c51195b8054bc9a1eb0f64a6c3
unknown <you@example.com>
2022-01-23 16:36:35 +0600

    hw_meder_6

---
+++ main.py
@@ -0,0 +1,13 @@
+class TimeDesk:
+    def __init__(self, seconds):
+        self.sec = seconds
+
+    def converter(self):
+            self.day = self.sec // 3600 // 24
+            self.hours = self.sec // 3600 - self.day * 24
+            self.minutes = self.sec % 3600 // 60
+            self.seconds = self.sec % 3600 % 60
+            return f'{self.day}ДНИ,{self.hours} ЧАСОВ, {self.minutes} МИНУТ, {self.seconds} СЕКУНД'
+
+time = TimeDesk(seconds=60)
+print(time.converter())
